dda260cfefa1fc2086fee7064cacbc382a2911c3
Fahad Guled <fahad.guled@yahoo.com>
2023-10-24 15:14:42 -0700

    added logout option and changed ui for login dashboard

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,6 +11,16 @@ import {
 } from "@nextui-org/navbar";
 import { Button } from "@nextui-org/button";
 import Link from "next/link";
+import Image from "next/image";
+import {
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+} from "@nextui-org/Dropdown";
+import { FcGoogle } from "react-icons/fc";
+import { BiLogInCircle } from "react-icons/bi";
+import { Avatar } from "@nextui-org/Avatar";
 import Themechanger from "../components/ThemeChanger";
 import { UserAuth } from "../context/AuthContext";
 
@@ -38,6 +48,16 @@ export default function App() {
       console.log(error);
     }
   };
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(user);
 
   return (
     <div className="mt-3 border-b-[1px] border-black dark:border-white border-opacity-20 dark:border-opacity-20">
@@ -72,16 +92,89 @@ export default function App() {
         <NavbarContent justify="end">
           <div className="flex w-[200px] justify-evenly">
             <Themechanger />
-            <NavbarItem className="hidden lg:flex">
-              <Link href="#" onClick={handleSignIn}>
-                Login
-              </Link>
-            </NavbarItem>
-            <NavbarItem>
-              <Button as={Link} color="primary" href="#" variant="flat">
-                Sign Up
-              </Button>
-            </NavbarItem>
+            {!user ? (
+              <div className="flex">
+                <NavbarItem className="flex mr-4">
+                  <Link href="#" onClick={handleSignIn}>
+                    <FcGoogle size={30} />
+                  </Link>
+                </NavbarItem>
+                <NavbarItem>
+                  <Button as={Link} color="primary" href="#" variant="flat">
+                    <BiLogInCircle size={30} />
+                  </Button>
+                </NavbarItem>
+              </div>
+            ) : (
+              <Dropdown placement="bottom-end">
+                <DropdownTrigger>
+                  <div className="flex items-center">
+                    <div className="mr-2">{user.displayName}</div>
+                    <Image
+                      src={user.photoURL}
+                      className="rounded-full cursor-pointer"
+                      alt="user-image"
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                </DropdownTrigger>
+                <DropdownMenu
+                  aria-label="Profile Actions"
+                  variant="flat"
+                  className="dark:bg-[#151515] bg-[#e0dfdf] p-2 rounded-xl"
+                >
+                  <DropdownItem key="profile" className="h-14 gap-2">
+                    <p className="font-semibold">Signed in as</p>
+                    <p className="font-semibold">{user.email}</p>
+                  </DropdownItem>
+                  <DropdownItem
+                    className="dark:hover:bg-[#242424] hover:bg-[#c3c2c2] "
+                    key="settings"
+                  >
+                    My Settings
+                  </DropdownItem>
+                  <DropdownItem
+                    className="dark:hover:bg-[#242424] hover:bg-[#c3c2c2] cursor-pointer "
+                    key="team_settings"
+                  >
+                    Team Settings
+                  </DropdownItem>
+                  <DropdownItem
+                    className="dark:hover:bg-[#242424] hover:bg-[#c3c2c2] cursor-pointer "
+                    key="analytics"
+                  >
+                    Analytics
+                  </DropdownItem>
+                  <DropdownItem
+                    className="dark:hover:bg-[#242424] hover:bg-[#c3c2c2] cursor-pointer "
+                    key="system"
+                  >
+                    System
+                  </DropdownItem>
+                  <DropdownItem
+                    className="dark:hover:bg-[#242424] hover:bg-[#c3c2c2] cursor-pointer "
+                    key="configurations"
+                  >
+                    Configurations
+                  </DropdownItem>
+                  <DropdownItem
+                    className="dark:hover:bg-[#242424] hover:bg-[#c3c2c2] cursor-pointer "
+                    key="help_and_feedback"
+                  >
+                    Help & Feedback
+                  </DropdownItem>
+                  <DropdownItem
+                    key="logout"
+                    color="danger"
+                    onClick={handleSignOut}
+                    className="dark:hover:bg-[#242424] hover:bg-[#c3c2c2] cursor-pointer "
+                  >
+                    Log Out
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            )}
           </div>
         </NavbarContent>
         <NavbarMenu>
